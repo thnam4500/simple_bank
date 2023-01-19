a750de7c549f5cd8dc965322c8a7a0fad79a858e
Nam Trần (Software Engineer Intern) <nam.tran@be.com.vn>
2023-01-19 15:14:56 +0700

    complete lecture 12

--- main.go
+++ main.go
@@ -7,22 +7,21 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/thnam4500/simple_bank/api"
 	db "github.com/thnam4500/simple_bank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/thnam4500/simple_bank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("error load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("error database connection", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
